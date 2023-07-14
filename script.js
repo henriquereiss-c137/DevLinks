63ad46b2a3b0d10093b852fb278a6f781e92a83e
Henrique Reis <henriquereiss.c137@gmail.com>
2023-07-14 10:57:22 -0300

    avatar light mode removed

--- script.js
+++ script.js
@@ -3,18 +3,4 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   html.classList.toggle("light")
-
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute(
-      "alt",
-      "foto de MaykBrito sorrindo usando óculos escuros, jaqueta cinza e camisa preta"
-    )
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute(
-      "alt",
-      "foto de MaykBrito sorrindo, usando óculos camisa preta e fundo amarelo"
-    )
-  }
 }
